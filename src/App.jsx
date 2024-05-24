68a2fe1b339b6cbc4054bb7898e7181e4b8fe893
Navindu Kavishka <navindukavishka2001@gmail.com>
2024-05-24 17:45:02 +0530

    admin sidebar first

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import AdminLogin from './pages/AdminLogin/AdminLogin'
+import React from 'react';
+import './components/Admin/SideBar/SideBar.css';
+import { BrowserRouter, Route ,Routes } from 'react-router-dom';
+import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
+import AdminDonor from "./pages/AdminDonor/AdminDonor";
+import AdminFundraiser from "./pages/AdminFundRaiser/AdminFundRaiser";
+import AdminProject from "./pages/AdminProject/AdminProject";
+import AdminPayment from "./pages/AdminPayment/AdminPayment";
+import SideBar from "./components/Admin/SideBar/SideBar"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <AdminLogin/>
-      
-    </>
-  )
-}
+    <BrowserRouter>
+    <SideBar>
+    <Routes>
+      <Route path='/' element={<AdminDashboard/>}/>
+      <Route path='/admindashboard' element={<AdminDashboard/>}/>
+      <Route path='/admindonor' element={<AdminDonor/>}/>
+      <Route path='/adminfundraiser' element={<AdminFundraiser/>}/>
+      <Route path='/adminproject' element={<AdminProject/>}/>
+      <Route path='/adminpayment' element={<AdminPayment/>}/>
 
+      
+    </Routes>
+    </SideBar>
+    </BrowserRouter>
+  );
+};
 export default App
