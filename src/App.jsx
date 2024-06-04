96898355b48040580b5c79bad96d407e3f0fe880
Navindu Kavishka <navindukavishka2001@gmail.com>
2024-06-04 11:55:39 +0530

    updated after dir piyumi merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+// No import needed since React is not used in this file
 import './components/Admin/SideBar/SideBar.css';
 import { BrowserRouter, Route ,Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
